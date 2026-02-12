97c562edc4181b60fd24d0e0be44e0de1fde3f9a
Py-Swift <psychowaspx@gmail.com>
2026-02-12 11:37:01 +0100

    Initial commit

---
+++ Sources/Backends/Backends.swift
@@ -0,0 +1,98 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+import PSTools
+
+
+public struct WrapperImporter: Decodable {
+    
+    public let libraries: [Library]
+    public let modules: [WrapperImport]
+    
+    public init(libraries: [Library], modules: [WrapperImport]) {
+        self.libraries = libraries
+        self.modules = modules
+    }
+    
+    enum CodingKeys: CodingKey {
+        case libraries
+        case modules
+    }
+    
+    public init(from decoder: any Decoder) throws {
+        let container = try! decoder.container(keyedBy: CodingKeys.self)
+        self.libraries = try! container.decode([WrapperImporter.Library].self, forKey: .libraries)
+        self.modules = try! container.decode([WrapperImporter.WrapperImport].self, forKey: .modules)
+    }
+   
+    
+    public struct Library: CustomStringConvertible, Decodable {
+        
+        public let name: String
+        
+        public init(name: String) {
+            self.name = name
+        }
+        
+        public var description: String {
+            name
+        }
+        
+        public init(from decoder: any Decoder) throws {
+            let c = try decoder.singleValueContainer()
+            name = try c.decode(String.self)
+        }
+    }
+    
+    public enum WrapperImport: CustomStringConvertible, Decodable {
+        
+        case static_import(String)
+        case name_import(name: String, module: String)
+        
+        enum NameImportCodingKeys: CodingKey {
+            case name
+            case module
+        }
+        
+        public init(from decoder: any Decoder) throws {
+            if let c = try? decoder.singleValueContainer() {
+                let string = try! c.decode(String.self)
+                if string.hasPrefix(".") {
+                    self = .static_import(string)
+                } else {
+                    fatalError("raw strings should contain prefix . and refer to static extension")
+                }
+            } else {
+                let c = try! decoder.container(keyedBy: NameImportCodingKeys.self)
+                self = .name_import(
+                    name: try c.decode(String.self, forKey: .name),
+                    module: try c.decode(String.self, forKey: .module)
+                )
+            }
+        }
+        
+        
+        public var description: String {
+            switch self {
+                case .static_import(let string):
+                    string
+                case .name_import(let name, let module):
+                    ".init(name: \(name), module: \(module).py_init )"
+            }
+        }
+    }
+}
+
+extension Validation {
+    public static func supportPythonFramework() async throws {
+        if validateSupportPythonFramework() { return }
+        //let gil = PyGILState_Ensure()
+        let py_backend = await PyFrameworkBackend()
+        
+        try await py_backend.do_install(support: .ps_support, platform: .iOS)
+        try await py_backend.do_install(support: .ps_support, platform: .macOS)
+        
+        //PyGILState_Release(gil)
+    }
+}
+
+
---
+++ Sources/Backends/backends/PyFrameworkBackend.swift
@@ -0,0 +1,79 @@
+
+import Foundation
+import PathKit
+import ProjectSpec
+import PSTools
+
+public class PyFrameworkBackend: BackendProtocol {
+    
+    
+    
+    public var name: String { "PyFrameworkBackend"}
+    
+    let version = "3.13"
+    let sub_version = "b11"
+
+    public var app_name: String?
+    
+    
+    fileprivate func download(version: String, sub_version: String , platform: String, destination: Path ) async throws -> Path {
+        let url: URL = .init(string: "https://github.com/beeware/Python-Apple-support/releases/download/\(version)-\(sub_version)/Python-\(version)-\(platform)-support.\(sub_version).tar.gz")!
+        let (tmp, _) = try await URLSession.download(from: url)
+        let filename = "Python-\(version)-\(platform)-support.{\(sub_version).tar.gz"
+        let py_fw_tar = destination + filename
+        try tmp.move(py_fw_tar)
+        return py_fw_tar
+    }
+    @MainActor
+    public func install(support: Path, platform: Platform) async throws {
+        
+        
+        
+        let _support = Path.ps_support
+        let py_fw  = _support + "Python.xcframework"
+        
+        
+        
+        
+        
+        switch platform {
+                
+            case .iOS:
+                if py_fw.exists { return }
+                let py_fw_tar = try await download(
+                    version: version,
+                    sub_version: sub_version,
+                    platform: platform.rawValue,
+                    destination: _support
+                )
+                try _support.chdir {
+                    
+                    try Process.untar(url: py_fw_tar)
+                    try py_fw_tar.delete()
+                }
+            case .macOS:
+                let py_fw_tar = try await download(
+                    version: version,
+                    sub_version: sub_version,
+                    platform: platform.rawValue,
+                    destination: _support
+                )
+                try _support.chdir {
+                    try Process.untar(url: py_fw_tar)
+                    try py_fw_tar.delete()
+                }
+            case .auto:
+                break
+                
+            case .tvOS, .watchOS, .visionOS:
+                break
+        }
+        
+        
+    }
+}
+
+
+
+
+
